8e69dc858ee48c041c108c5142a094373421c60c
Shahriar Rumi Dipto <rumi.dipto@therapservices.net>
2022-07-28 09:41:50 +0600

    Initialized the project with required dependencies

---
+++ src/main/java/net/therap/reactwithspringbasics/ReactwithspringbasicsApplication.java
@@ -0,0 +1,13 @@
+package net.therap.reactwithspringbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactwithspringbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactwithspringbasicsApplication.class, args);
+	}
+
+}
