39863a087f895d917d52528c2797e33c1282f18f
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2022-08-01 12:45:39 +0600

    Added method to convert UserDto to User

--- src/main/java/net/therap/reactwithspringbasics/util/UserDtoMapper.java
+++ src/main/java/net/therap/reactwithspringbasics/util/UserDtoMapper.java
@@ -1,7 +1,10 @@
 package net.therap.reactwithspringbasics.util;
 
 import net.therap.reactwithspringbasics.DTO.UserDto;
+import net.therap.reactwithspringbasics.domain.Designation;
 import net.therap.reactwithspringbasics.domain.User;
+
+import java.math.BigDecimal;
 
 /**
  * @author rumi.dipto
@@ -9,9 +12,18 @@ import net.therap.reactwithspringbasics.domain.User;
  */
 public class UserDtoMapper {
 
-    public static UserDto convertToUserDto(User user) {
+    public static UserDto convertUserToUserDto(User user) {
         return new UserDto(user.getFirstName(),
                 user.getLastName(),
                 user.getUsername());
+    }
+
+    public static User convertUserDtoToUser(UserDto userDto) {
+        return new User(userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getUsername(),
+                "pass",
+                Designation.TEAM_LEAD,
+                new BigDecimal(20000.50));
     }
 }
